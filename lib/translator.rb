e8aec305741fe042b72f2b502c84c54748ed4cfb
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-17 05:32:22 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,10 @@ require "yaml"
 
 def load_library path
   dict = YAML.load_file path
-  
+  ret = {'get_meaning' => {}, 'get_emoticon' => {}}
+  dict.each do |meaning, emotes|
+    ret[]
+  end
 end
 
 def get_japanese_emoticon
